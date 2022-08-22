46c9c52b20dbda49ce1322f2fc83fdcaa3418a6d
bsmmx <2295856084@qq.com>
2022-08-22 14:12:55 +0800

    1

---
+++ imall-mbg/src/main/java/com/situ/mall/mapper/OrderMapper.java
@@ -0,0 +1,30 @@
+package com.situ.mall.mapper;
+
+import com.situ.mall.pojo.Order;
+import com.situ.mall.pojo.OrderExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface OrderMapper {
+    long countByExample(OrderExample example);
+
+    int deleteByExample(OrderExample example);
+
+    int deleteByPrimaryKey(Long orderNo);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    List<Order> selectByExample(OrderExample example);
+
+    Order selectByPrimaryKey(Long orderNo);
+
+    int updateByExampleSelective(@Param("record") Order record, @Param("example") OrderExample example);
+
+    int updateByExample(@Param("record") Order record, @Param("example") OrderExample example);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
---
+++ imall-mbg/src/main/java/com/situ/mall/mapper/ShippingMapper.java
@@ -0,0 +1,30 @@
+package com.situ.mall.mapper;
+
+import com.situ.mall.pojo.Shipping;
+import com.situ.mall.pojo.ShippingExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ShippingMapper {
+    long countByExample(ShippingExample example);
+
+    int deleteByExample(ShippingExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    List<Shipping> selectByExample(ShippingExample example);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Shipping record, @Param("example") ShippingExample example);
+
+    int updateByExample(@Param("record") Shipping record, @Param("example") ShippingExample example);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+}
